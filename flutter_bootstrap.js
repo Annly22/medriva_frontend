d0978ff0cf5fdcdbe960bd9c4a8e6b2b446b13d9
Annly22 <annreebaneil@gmail.com>
2026-03-29 19:18:42 +0530

    frontend deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "687153581"
+    serviceWorkerVersion: "238372373"
   }
 });
